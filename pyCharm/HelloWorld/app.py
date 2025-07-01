636520e8367182526539b57b4f53c636968c3ef0
untalmike <miguel.gonzalezl@hotmail.com>
2025-07-01 15:38:36 -0600

    For loops, lists, while, if sentences, and references

--- pyCharm/HelloWorld/app.py
+++ pyCharm/HelloWorld/app.py
@@ -66,4 +66,63 @@ is_online = True
 #     print("It's a lovely day")
 # print("Enjoy your day!")
 
+# Ejercicio peso:
+# weight = input("Weight: ")
+# unit = input("¿Libras o kilos? (l/k): ")
+# if unit.upper() == "K":
+#     converted = float(weight) / 0.45
+#     print("Weight in Lbs: " + str(converted))
+# elif unit.upper() == "L":
+#     converted = float(weight) * 0.45
+#     print("Weight in Kgs: " + str(converted))
+# else:
+#     print("Not allowed option. Please enter 'l' for pounds or 'k' for kilos.")
 
+# while loops
+# i = 1
+# while i <= 10:
+#     print(i * '*')
+#     i += 1  # Incrementa i en 1
+
+# Lista de números
+# names = ["John", "Mary", "Bob", "Alice", "Sam"]
+# names[0] = "Jon"
+# print("Names:", names)
+# print("First name:", names[0])  # Acceso al primer elemento
+# print("Last name:", names[-1])  # Acceso al último elemento
+# print(names[0:3])  # Acceso a un rango de elementos
+
+# List methods
+# numbers = [1, 2, 3, 4, 5]
+# numbers.append(6)  # Añade un elemento al final
+# numbers.insert(0, 0)  # Inserta un elemento en la posición 0
+# numbers.remove(3)  # Elimina el primer elemento con valor 3
+# numbers.pop()  # Elimina el último elemento
+# numbers.clear()  # Limpia la lista
+# numbers.sort()  # Ordena la lista
+# numbers.reverse()  # Invierte el orden de la lista
+# numbers_copy = numbers.copy()  # Crea una copia de la lista
+# print("Numbers:", numbers)
+# print("Numbers copy:", numbers_copy)
+# print(len(numbers))  # Longitud de la lista
+
+# bucles for
+# numbers = [1, 2, 3, 4, 5]
+# for item in numbers:
+#     print("Number:", item)
+
+# i = 0
+# while i < len(numbers):
+#     print("Number:", numbers[i])
+#     i += 1  # Incrementa i en 1
+
+# función range
+
+# numbers_range = range(5, 10, 2)  # Crea un rango de números del 5 al 9 (el 10 no se incluye) luego realiza un salto de 2
+# for number in range(5):
+#     print("Number from range:", number)
+
+# tuple
+coordinates = (10, 20, 30)  # Tupla de coordenadas, lleva corchetes y no se puede modificar
+# coordinates[0] = 20  # Esto generará un error porque las tuplas son inmutables
+print("Coordinates:", coordinates)
